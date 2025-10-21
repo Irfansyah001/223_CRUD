1deb8ba40bf05d5a808cebb7d7fe859fff98ddaf
Irfansyah001 <syahirfan545@gmail.com>
2025-10-21 10:02:01 +0700

    add - creating standard server on index.js with importing express and mysql module etc., defined a basic route, and createing connection to the database

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express'); //import express module
+let mysql = require('mysql2'); //import mysql module
+const app = express(); //create an express application
+const port = 3000; //set the port number
+app.use(express.json()); //middleware to parse JSON request body
+app.use(express.urlencoded({ extended: true })); //middleware to parse URL-encoded request body
+
+app.get('/', (req, res) => { //define a route for the root URL
+  res.send('Hello World!'); //send a response
+});
+
+app.listen(port, () => { //start the server
+  console.log(`Server is running on port ${port}`); //log a message to the console
+});
+
+const db = mysql.createConnection({ //create a connection to the database
+  host: 'localhost', //database host
+  user: 'root', //database user
+  password: '', //database password
+  database: 'biodata', //database name
+
+});
